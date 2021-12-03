1fd215f230d5361d551a47a5f5de3f16e56a7481
scolpig <scolpig@gmail.com>
2021-12-03 10:09:41 +0900

    concat 01

--- job02_concat.py
+++ job02_concat.py
@@ -5,9 +5,15 @@ df.info()
 
 for i in range(2, 38):
     df_temp = pd.read_csv(
-        './crawling_data/reviews_2020_{}.csv'.format(i),
-        index_col=0)
-
+        './crawling_data/reviews_2020_{}.csv'.format(i))
+    df_temp.dropna(inplace=True)
+    df_temp.drop_duplicates()
+    df_temp.columns = ['title','reviews']
+    df_temp.to_csv('./crawling_data/reviews_2020_{}.csv'.format(i),
+                   index=False)
+    df = pd.concat([df, df_temp], ignore_index=True)
+df.info()
+df.to_csv('./crawling_data/reviews_2020.csv', index=False)
 
 
 
