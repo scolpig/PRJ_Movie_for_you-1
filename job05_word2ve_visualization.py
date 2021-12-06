61389a138689f36f08131c3c03eb215a1cc683e5
scolpig <scolpig@gmail.com>
2021-12-06 12:30:49 +0900

    visualization 02

--- job05_word2ve_visualization.py
+++ job05_word2ve_visualization.py
@@ -5,3 +5,37 @@ from sklearn.manifold import TSNE
 from matplotlib import font_manager, rc
 import matplotlib as mpl
 
+font_path = './malgun.ttf'
+font_name = font_manager.FontProperties(
+    fname=font_path).get_name()
+mpl.rcParams['axes.unicode_minus']=False
+rc('font', family=font_name)
+
+embedding_model = Word2Vec.load('./models/word2VecModel_2015_2021.model')
+key_word = '일요일'
+sim_word = embedding_model.wv.most_similar(key_word, topn=10)
+print(sim_word)
+
+vectors = []
+labels = []
+for label, _ in sim_word:
+    labels.append(label)
+    vectors.append(embedding_model.wv[label])
+print(vectors[0])
+print(len(vectors[0]))
+
+df_vectors = pd.DataFrame(vectors)
+print(df_vectors.head())
+
+tsne_model = TSNE(perplexity=40, n_components=2,
+                  init='pca', n_iter=2500, random_state=23)
+new_value = tsne_model.fit_transform(df_vectors)
+df_xy = pd.DataFrame({'words':labels,
+                      'x':new_value[:, 0],
+                      'y':new_value[:, 1]})
+print(df_xy.head())
+
+
+
+
+
